b5b0c49a964e7cd356a7152869abca256b6e6680
Your Name <yraghu13@gmail.com>
2020-11-06 23:10:38 +0100

    Added Accuracy

--- Regression/MulRegRoomPrice.py
+++ Regression/MulRegRoomPrice.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from word2number import w2n
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 df = pd.read_csv(r'C:\Users\telelink\Contacts\Desktop\hiring.csv')
 df.experience = df.experience.fillna("zero")
@@ -16,6 +17,7 @@ reg = linear_model.LinearRegression()
 reg.fit(df[['experience', 'test_score', 'sportScore\t']], df['salary'])
 print('coefficient:', reg.coef_ ,'intercept:', reg.intercept_)
 #plt.plot(df.experience, reg.predict(df[['experience']]), '-b')
-print(reg.predict([[2,9,6]]))
+print(reg.predict([[12, 10, 10]]))
+print("R2-score: %.2f" % r2_score(df.salary, reg.predict(df[['experience', 'test_score', 'sportScore\t']])))
 
 plt.show()
